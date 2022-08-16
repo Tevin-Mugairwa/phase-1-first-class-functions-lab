a0892138d51ef21b6c9272fc4ec2b0a9824887a8
Tevin-Mugairwa <tevin.mugairwa@student.moringaschool.com>
2022-08-16 18:10:43 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function anonymousFunction([]){
+    return (['Antonia', 'Nuru']);
+    
+    }
+    
+    const  returnLastTwoDrivers = function anonymousFunction([]){
+        return (['Amari', 'Mo']);
+    
+    }
+    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+    
+    function createFareMultiplier(integer){
+        return function(price){
+          return price*integer;
+        };
+    }
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    
+    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+        return driversToReturn(arrayOfDrivers);
+    }
